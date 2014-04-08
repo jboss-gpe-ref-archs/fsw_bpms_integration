60ec4eb1f528af3683abdd3a5ba1690ae98007b7
ratwater <jbride2001@yahoo.com>
2014-04-08 17:05:41 -0600

    changed namespace and package names

--- serviceTier/src/main/java/com/example/switchyard/serviceTier/CompleteTask.java
+++
@@ -1,5 +0,0 @@
-package com.example.switchyard.serviceTier;
-
-public interface CompleteTask {
-
-}
---
+++ serviceTier/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/PolicyQuoteProcessLifeCycleManagementBean.java
@@ -0,0 +1,9 @@
+package com.redhat.gpe.refarch.fsw_bpms_integration.serviceTier;
+
+import org.switchyard.component.bean.Service;
+
+@Service(PolicyQuoteProcessLifeCycleManagement.class)
+public class PolicyQuoteProcessLifeCycleManagementBean implements
+		PolicyQuoteProcessLifeCycleManagement {
+
+}
