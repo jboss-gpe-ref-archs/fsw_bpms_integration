dc6fdcf3ae611aba960f91b1f78f6570955e2d49
ratwater <jbride2001@yahoo.com>
2014-04-20 11:08:07 -0600

    now getting processId back and processing via xpath

--- serviceTier/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/ProcessMgmtBean.java
+++ serviceTier/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/ProcessMgmtBean.java
@@ -1,15 +1,25 @@
 package com.redhat.gpe.refarch.fsw_bpms_integration.serviceTier;
 
+import java.io.ByteArrayInputStream;
 import java.io.StringWriter;
 import java.util.Random;
-import javax.inject.Inject;
 
+import javax.inject.Inject;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
+import org.switchyard.Context;
 import org.switchyard.component.bean.Reference;
 import org.switchyard.component.bean.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.apache.log4j.Logger;
 
 import com.redhat.gpe.refarch.fsw_bpms_integration.domain.ProcessDetails;
@@ -18,8 +28,15 @@ import com.redhat.gpe.refarch.fsw_bpms_integration.domain.Policy;
 @Service(ProcessMgmt.class)
 public class ProcessMgmtBean implements ProcessMgmt {
 
+	private static final String BPMS_EXEC_SERVER_HOSTNAME = "bpms.exec.server.hostname";
+	private static final String BPMS_EXEC_SERVER_PORT = "bpms.exec.server.port";
+	private static final String START_PROCESS_ID_PATH = "/process-instance/id";
     private static Logger log = Logger.getLogger("ProcessMgmtBean");
     private static Random random = new Random();
+    
+    
+    @Inject
+    private Context context;
     
     @Inject @Reference("StartProcess")
     private StartProcess sProcessAction;
@@ -27,13 +44,24 @@ public class ProcessMgmtBean implements ProcessMgmt {
     @Inject @Reference("GetPotentialTasks")
     private GetPotentialTasks gPotentialTasks;
 
+    public ProcessMgmtBean() {
+    	log.info("ProcessMgmtBean() ****** ");
+    }
+
     public void executeProcessLifecycle(ProcessDetails pDetails) {
+    	StringBuilder sBuilder = new StringBuilder("executeProcessLifecycle() props = \n\t");
+    	sBuilder.append("bpms.exec.server.hostname= ");
+    	sBuilder.append(context.getPropertyValue(BPMS_EXEC_SERVER_HOSTNAME));
+    	sBuilder.append("\n\tbpms.exec.server.port=");
+    	sBuilder.append(context.getPropertyValue(BPMS_EXEC_SERVER_PORT));
+    	log.info(sBuilder.toString());
         log.info("executeProcessLifecycle() pDetails = "+pDetails);
       
         try { 
             String policyJaxb = getPolicyJaxb(); 
-            String response = sProcessAction.start(policyJaxb);
-            log.info("executeProcessLifecycle() startProcess response = "+response);
+            String httpEntity = sProcessAction.start(policyJaxb);
+            String processId = getProcessId(httpEntity);
+            
 
             //gPotentialTasks.getPotentialTasks();
         }catch(Throwable x) {
@@ -49,5 +77,29 @@ public class ProcessMgmtBean implements ProcessMgmt {
         StringWriter sw = new StringWriter();
         jaxbMarshaller.marshal(policyObj, sw);
         return sw.toString();
+    }
+    
+    private String getProcessId(String httpEntity) throws Exception {
+    	/*
+    	 * <process-instance>
+    	 *   <status>SUCCESS</status>
+    	 *   <url>http://bpmsjb-bpmstraining.apps.lab2.opentlc.com/business-central/rest/runtime/com.redhat.gpe.refarch.fsw_bpms_integration:processTier:1.0/process/processTier.policyQuoteProcess/start</url>
+    	 *   <process-id>processTier.policyQuoteProcess</process-id>
+    	 *   <id>3</id>
+    	 *   <state>1</state>
+    	 * </process-instance>
+    	 */
+    	log.info("getProcessId() entity = "+httpEntity);
+    	
+    	// wouldn't be a bad idea to pool these compiled xpathExpressions along with DocumentBuilderFactory
+    	XPath xpath = XPathFactory.newInstance().newXPath();
+    	XPathExpression startProcessXPath = xpath.compile(START_PROCESS_ID_PATH);
+    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    	DocumentBuilder dBuilder = factory.newDocumentBuilder();
+    	Document httpEntityDoc = dBuilder.parse(new ByteArrayInputStream(httpEntity.getBytes()));
+    	
+    	Node pInstanceId = (Node)startProcessXPath.evaluate(httpEntityDoc.getDocumentElement(), XPathConstants.NODE);
+    	log.info("getProcessId() processId = "+pInstanceId.getTextContent());
+    	return pInstanceId.getTextContent();
     }
 }
---
+++ serviceTier/src/test/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/ProcessMgmtTest.java
@@ -0,0 +1,74 @@
+package com.redhat.gpe.refarch.fsw_bpms_integration.serviceTier;
+
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.junit.Assert;
+import org.switchyard.component.test.mixins.http.HTTPMixIn;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+/**
+ *  Example invocation:   
+ *    mvn clean test exec:java -Dbpms.exec.server.hostname=bpmsjb-bpmstraining.apps.lab2.opentlc.com -Dbpms.exec.server.port=80
+ */
+public class ProcessMgmtTest {
+
+    private static final String UTF_8 = "UTF-8";
+    private static final String BPMS_EXEC_SERVER_HOSTNAME = "bpms.exec.server.hostname";
+    private static final String BPMS_EXEC_SERVER_PORT = "bpms.exec.server.port";
+    private static final String RUNTIME_CONTEXT = "runtime/";
+    private static final String TASK_CONTEXT = "task/";
+    private static final String DEPLOYMENT_ID = "com.redhat.gpe.refarch.fsw_bpms_integration:processTier:1.0";
+    private static final String PROCESS_ID = "processTier.policyQuoteProcess";
+    private static final String TEST_START_PROCESS_RESPONSE = "<process-instance><status>SUCCESS</status><url>http://bpmsjb-bpmstraining.apps.lab2.opentlc.com/business-central/rest/runtime/com.redhat.gpe.refarch.fsw_bpms_integration:processTier:1.0/process/processTier.policyQuoteProcess/start</url><process-id>processTier.policyQuoteProcess</process-id><id>13</id><state>1</state></process-instance>";
+
+    public static void main(String[] args) throws Exception {
+        //testStartProcess();
+        testDocAndXPath();
+    }
+
+    private static void testStartProcess() throws Exception {
+        String host = System.getProperty(BPMS_EXEC_SERVER_HOSTNAME, "localhost");
+        int port = Integer.parseInt(System.getProperty(BPMS_EXEC_SERVER_PORT, "8080"));
+        String baseUrl="http://"+host+":"+port+"/business-central/rest/";
+        System.out.println("main() baseUrl = "+baseUrl);
+        HTTPMixIn http = new HTTPMixIn();
+        http.initialize();
+        http.setContentType("application/x-www-form-urlencoded");
+        int status = http.sendStringAndGetStatus(baseUrl + RUNTIME_CONTEXT+DEPLOYMENT_ID+"/process/"+PROCESS_ID+"/start", "", HTTPMixIn.HTTP_POST);
+        Assert.assertEquals(401, status);
+        http.setRequestHeader("Authorization", "Basic amJvc3M6YnJtcw=");
+        HttpResponse response = http.sendStringAndGetMethod(baseUrl + RUNTIME_CONTEXT+DEPLOYMENT_ID+"/process/"+PROCESS_ID+"/start", "", HTTPMixIn.HTTP_POST);
+        System.out.println("main() startProcess statusLine = "+response.getStatusLine());
+        String responseString = EntityUtils.toString(response.getEntity(), UTF_8);
+        //System.out.println("main() startProcess response = "+responseString);
+    }
+
+    private static void testDocAndXPath() throws Exception {
+        
+    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    	DocumentBuilder dBuilder = factory.newDocumentBuilder();
+    	Document httpEntityDoc = dBuilder.parse((new InputSource(new StringReader(TEST_START_PROCESS_RESPONSE))));
+    	String nodeValue = httpEntityDoc.getElementsByTagName("id").item(0).getTextContent();
+    	System.out.println("firstNode = "+nodeValue);
+    	
+    	XPath xpath = XPathFactory.newInstance().newXPath();
+    	XPathExpression startProcessXPath = xpath.compile("/process-instance/id");
+    	Node pInstanceId = (Node)startProcessXPath.evaluate(httpEntityDoc.getDocumentElement(), XPathConstants.NODE);
+    	
+        System.out.println("testXPath() pInstanceId = "+pInstanceId.getTextContent());
+    }
+}
