8cb1c0a01bf9cdc4ebee453f3a6d809f795f0e02
ratwater <jbride2001@yahoo.com>
2014-05-06 10:13:04 -0600

    PolicyMgmtComposer now mashals string payload to a Policy object

--- serviceTier/policyQuote/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/PolicyMgmtComposer.java
+++ serviceTier/policyQuote/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/PolicyMgmtComposer.java
@@ -1,5 +1,8 @@
 package com.redhat.gpe.refarch.fsw_bpms_integration.serviceTier;
 
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import org.switchyard.Exchange;
 import org.switchyard.Message;
 import org.switchyard.component.resteasy.composer.RESTEasyBindingData;
@@ -9,11 +12,21 @@ import com.redhat.gpe.refarch.fsw_bpms_integration.domain.Policy;
 
 public class PolicyMgmtComposer extends RESTEasyMessageComposer{
 	
+    private Logger log = Logger.getLogger("PolicyMgmtComposer");
+    private ObjectMapper jsonMapper = new ObjectMapper();
+	
 	public Message compose(RESTEasyBindingData source, Exchange exchange) throws Exception {
         final Message message = super.compose(source, exchange);
-        Policy pObj = new Policy(1, "2");
-        message.setContent(pObj);
-        return message;
+        String content = null;
+        try {
+        	content = (String)message.getContent();
+        	Policy pObj = jsonMapper.readValue(content, Policy.class);
+        	message.setContent(pObj);
+        	return message;
+        }catch(Exception x){
+        	log.error("compose() content = "+message.getContent()+" : exception = "+x.getLocalizedMessage());
+        	throw x;
+        }
     }
 
 }
