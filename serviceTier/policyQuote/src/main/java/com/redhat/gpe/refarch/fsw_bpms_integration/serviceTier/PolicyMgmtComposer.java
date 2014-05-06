d6c1afe9ec659329c07cfdd19272a590a7517f88
ratwater <jbride2001@yahoo.com>
2014-05-06 15:09:25 -0600

    workaround for Content mapping problem

--- serviceTier/policyQuote/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/PolicyMgmtComposer.java
+++ serviceTier/policyQuote/src/main/java/com/redhat/gpe/refarch/fsw_bpms_integration/serviceTier/PolicyMgmtComposer.java
@@ -11,22 +11,26 @@ import org.switchyard.component.resteasy.composer.RESTEasyMessageComposer;
 import com.redhat.gpe.refarch.fsw_bpms_integration.domain.Policy;
 
 public class PolicyMgmtComposer extends RESTEasyMessageComposer{
-	
+    
     private Logger log = Logger.getLogger("PolicyMgmtComposer");
     private ObjectMapper jsonMapper = new ObjectMapper();
-	
-	public Message compose(RESTEasyBindingData source, Exchange exchange) throws Exception {
+    
+    public Message compose(RESTEasyBindingData source, Exchange exchange) throws Exception {
         final Message message = super.compose(source, exchange);
         String content = null;
+        Policy pObj = null;
         try {
-        	content = (String)message.getContent();
-        	Policy pObj = jsonMapper.readValue(content, Policy.class);
-        	message.setContent(pObj);
-        	return message;
+            content = (String)message.getContent();
+            pObj = jsonMapper.readValue(content, Policy.class);
         }catch(Exception x){
-        	log.error("compose() content = "+message.getContent()+" : exception = "+x.getLocalizedMessage());
-        	throw x;
+            log.error("compose() content = "+message.getContent()+" : exception = "+x.getLocalizedMessage());
+            log.error("compose() Root cause is that 'policyString' pInstanceVariable is not getting mapped correctly to 'Content' taskVariable in Send Results' node of  policyQuoteProcessMap");
+            log.error("compose() will just create a bogus Policy for now and troubleshoot later");
+            pObj = new Policy(1, "bogusPolicy");
+            
         }
+        message.setContent(pObj);
+        return message;
     }
 
 }
